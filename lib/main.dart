e3eca78f2d79fdc5827248a9f2e2dd8189dcdfd2
Harshita Anand <aharshita16@gmail.com>
2021-03-03 23:26:35 +0530

    main changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
-import 'package:brew_crew/screens/wrapper.dart';
-import 'package:brew_crew/services/auth.dart';
+import 'package:domestic_pal/screens/wrapper.dart';
+//import 'package:brew_crew/services/auth.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:brew_crew/models/user.dart';
+//import 'package:provider/provider.dart';
+//import 'package:brew_crew/models/user.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,12 +12,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return StreamProvider<User>.value(   //(the User) listening to stream
-      value: AuthService().user,   //what stream is going to listen to
-        child: MaterialApp(
+    return StreamProvider<User>.value(
+      //(the User) listening to stream
+      value: AuthService().user, //what stream is going to listen to
+      child: MaterialApp(
         home: Wrapper(),
       ),
-    );  
+    );
   }
 }
-
