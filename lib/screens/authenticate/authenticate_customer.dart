ade6e8a4686134f864ab2a26758afa08b00ba7f1
Harshita-Jaya <jharshita7991@gmail.com>
2021-03-03 23:01:11 +0530

    m

---
+++ lib/screens/authenticate/authenticate_customer.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AuthenticateCustomer extends StatefulWidget {
+  @override
+  _AuthenticateCustomerState createState() => _AuthenticateCustomerState();
+}
+
+class _AuthenticateCustomerState extends State<AuthenticateCustomer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Authenticate'),
+    );
+  }
+}
