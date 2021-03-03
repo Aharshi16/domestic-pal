da2cfbfd9822ce5a46b962c14136a48ee3d64057
Harshita-Jaya <jharshita7991@gmail.com>
2021-03-03 23:48:12 +0530

    anon sign in trial

---
+++ lib/screens/authenticate/login.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey[500],
+        elevation: 0.0,
+        title: Text('LOGIN SCREEN'),
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+        child: RaisedButton(
+          child: Text('Sign in anon'),
+          onPressed: () async{
+            
+          },
+        ),
+      ),
+    );
+  }
+}
