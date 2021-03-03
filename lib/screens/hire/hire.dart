c21b391500379c477248a6a0412ded80d9f5ab5e
Harshita Anand <aharshita16@gmail.com>
2021-03-03 23:50:45 +0530

    basic changes

--- lib/screens/hire/hire.dart
+++ lib/screens/hire/hire.dart
@@ -1,1 +1,10 @@
 import 'package:flutter/material.dart';
+
+class Hire extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('hire'),
+    );
+  }
+}
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:domestic_pal/screens/hire/hire.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    
+    return Hire();
   }
 }
