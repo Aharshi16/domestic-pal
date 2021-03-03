824d387f0da95ce231c2eda620604a89a9da3256
Harshita Anand <aharshita16@gmail.com>
2021-03-03 23:44:01 +0530

    home error removed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:domestic_pal/screens/wrapper.dart';
+import 'package:domestic_pal/screens/hire/hire.dart';
 //import 'package:brew_crew/services/auth.dart';
+import 'package:domestic_pal/screens/wrapper.dart';
 import 'package:flutter/material.dart';
 //import 'package:provider/provider.dart';
 //import 'package:brew_crew/models/user.dart';
@@ -12,12 +13,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return StreamProvider<User>.value(
-      //(the User) listening to stream
-      value: AuthService().user, //what stream is going to listen to
-      child: MaterialApp(
-        home: Wrapper(),
-      ),
+    return MaterialApp(
+      home: Wrapper(),
     );
   }
 }
