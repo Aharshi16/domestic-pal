1d754f5d09559c95ab8bccbec7859f2d1939f319
Harshita-Jaya <jharshita7991@gmail.com>
2021-03-04 22:48:33 +0530

    wrapper set

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:domestic_pal/models/user.dart';
+//import 'package:domestic_pal/screens/authenticate/signin_employee.dart';
 //import 'package:domestic_pal/screens/authenticate/signin_customer.dart';
 import 'package:domestic_pal/screens/wrapper.dart';
 import 'package:domestic_pal/services/auth_customer.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
 
 void main() => runApp(MyApp());
 
