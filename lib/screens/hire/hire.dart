ea9a1efdecf50cf77fe6d06c804749a0184e1462
Harshita Anand <aharshita16@gmail.com>
2021-03-04 20:01:04 +0530

    hire design trial

--- lib/screens/hire/hire.dart
+++ lib/screens/hire/hire.dart
@@ -3,8 +3,37 @@ import 'package:flutter/material.dart';
 class Hire extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('hire'),
-    );
+    return Scaffold(
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        //crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'Do you want to:',
+            style: TextStyle(fontSize: 30.0),
+          ),
+          SizedBox(height: 20.0),
+          FlatButton(
+            onPressed: () {},
+            minWidth: 115.0,
+            color: Colors.red[800],
+            child: Text(
+              'Hire',
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          FlatButton(
+            onPressed: () {},
+            minWidth: 60.0,
+            color: Colors.amber[800],
+            child: Text(
+              'Get hired',
+              style: TextStyle(fontSize: 20.0),
+            ),
+          )
+        ],
+      ),
+    ));
   }
 }
