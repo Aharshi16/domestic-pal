c3d4c950981de97c52af58d22af075bb97881bfc
Harshita-Jaya <jharshita7991@gmail.com>
2021-03-04 17:10:21 +0530

    customer model created

--- lib/screens/authenticate/signin_customer.dart
+++ lib/screens/authenticate/signin_customer.dart
@@ -26,7 +26,7 @@ class _SignInState extends State<SignIn> {
             if (result == null) {
               print('error ');
             } else {
-              print(result);
+              print(result.uid);
             }
           },
         ),
--- lib/services/auth_customer.dart
+++ lib/services/auth_customer.dart
@@ -1,14 +1,21 @@
+import 'package:domestic_pal/models/customer.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
   final FirebaseAuth _authCustomer = FirebaseAuth.instance;
+
+  //create user object on FIrebaeUser
+  Customer _userFromFirebase(FirebaseUser user){
+    return user != null ? Customer(uid: user.uid) : null;
+  }
 
   //sign in anon
   Future signInAnon() async {
     try {
       AuthResult result = await _authCustomer.signInAnonymously();
       FirebaseUser user = result.user;
-      return user;
+
+      return _userFromFirebase(user);
     } catch (e) {
       print(e.toString());
       return null;
