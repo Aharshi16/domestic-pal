ef7ef61b3bd20716255e49bd416ee47b8303da4e
jojo <jyotshanasingh107@gmail.com>
2021-03-04 19:59:39 +0530

    wrapped

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
+import 'package:domestic_pal/models/customer.dart';
 import 'package:domestic_pal/screens/authenticate/signin_customer.dart';
 import 'package:domestic_pal/screens/wrapper.dart';
+import 'package:domestic_pal/services/auth_customer.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: SignIn(),
+    return StreamProvider<Customer>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        home: Wrapper(),
+      ),
     );
   }
 }
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,10 +1,17 @@
 import 'package:domestic_pal/screens/authenticate/authenticate_customer.dart';
+import 'package:domestic_pal/screens/authenticate/signin_customer.dart';
 import 'package:domestic_pal/screens/home_customer/home_customer.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:domestic_pal/models/customer.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return HomeCustomer();
+
+    final user = Provider.of<Customer>(context);
+    print(user);
+
+    return AuthenticateCustomer();
   }
 }
