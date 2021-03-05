be5f78279abed81887d584a35db5ffae269c9442
Harshita Anand <aharshita16@gmail.com>
2021-03-05 17:10:33 +0530

    sign out emp done

--- lib/screens/home_employee/home_employee.dart
+++ lib/screens/home_employee/home_employee.dart
@@ -1,10 +1,27 @@
+import 'package:domestic_pal/services/auth_employee.dart';
 import 'package:flutter/material.dart';
 
 class HomeEmployee extends StatelessWidget {
+  final AuthService _auth = AuthService();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Emp HomeScreen'),
+    return Scaffold(
+      backgroundColor: Colors.cyan[50],
+      appBar: AppBar(
+        title: Text('Employee home screen'),
+        backgroundColor: Colors.cyan[400],
+        elevation: 0.0,
+        actions: <Widget>[
+          FlatButton.icon(
+            icon: Icon(Icons.person),
+            label: Text('Logout'),
+            onPressed: () async {
+              await _auth.signOut();
+            },
+          )
+        ],
+      ),
     );
   }
 }
--- lib/services/auth_employee.dart
+++ lib/services/auth_employee.dart
@@ -5,23 +5,22 @@ class AuthService {
   final FirebaseAuth _authEmployee = FirebaseAuth.instance;
 
   //create user obj based on FirebaseUser
-  User _userFromFirebaseUser(FirebaseUser user){
-    return user != null ? User(uid: user.uid ,category:'E'): null;
+  User _userFromFirebaseUser(FirebaseUser user) {
+    return user != null ? User(uid: user.uid, category: 'E') : null;
   }
 
   //auth changes user stream
-  Stream<User> get user{
-    return _authEmployee.onAuthStateChanged
-        .map(_userFromFirebaseUser);
+  Stream<User> get user {
+    return _authEmployee.onAuthStateChanged.map(_userFromFirebaseUser);
   }
 
   //sign in anon
-  Future signInAnon() async{
-    try{
+  Future signInAnon() async {
+    try {
       AuthResult result = await _authEmployee.signInAnonymously();
-      FirebaseUser user=result.user;
+      FirebaseUser user = result.user;
       return _userFromFirebaseUser(user);
-    }catch(e){
+    } catch (e) {
       print(e.toString());
       return null;
     }
@@ -29,7 +28,15 @@ class AuthService {
 
   //sign in with email and password
 
-  //sign out
-
   //register with email and password
+
+  //Sign out
+  Future signOut() async {
+    try {
+      return await _authEmployee.signOut();
+    } catch (e) {
+      print(e.toString);
+      return null;
+    }
+  }
 }
