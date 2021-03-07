7c106d450a9467f749da7bceaf882833eca38970
Harshita Anand <aharshita16@gmail.com>
2021-03-07 21:41:54 +0530

    commit to avoid error

--- lib/screens/wrapper/wrapper.dart
+++ lib/screens/wrapper/wrapper.dart
@@ -4,9 +4,10 @@ import 'package:domestic_pal/models/user.dart';
 import 'package:domestic_pal/screens/hire/hire.dart';
 //import 'package:domestic_pal/screens/home_customer/home_customer.dart';
 //import 'package:domestic_pal/screens/home_employee/home_employee.dart';
-import 'package:domestic_pal/screens/wrapper/check_user_type.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
+import 'check_user_type.dart';
 
 class Wrapper extends StatelessWidget {
   @override
@@ -22,7 +23,11 @@ class Wrapper extends StatelessWidget {
       return HomeEmployee();
     }*/
 
-    //return (user == null) ? Hire() : CheckUserType(user: user,);
-    if (user == null) return Hire();
+    return (user == null)
+        ? Hire()
+        : CheckUserType(
+            user: user,
+          );
+    //if (user == null) return Hire();
   }
 }
