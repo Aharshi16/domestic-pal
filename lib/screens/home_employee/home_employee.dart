3732272005ac741fd1d5efd4f935dc97ff7a8ae3
Diptidiv <65387310+Diptidiv@users.noreply.github.com>
2021-03-28 16:56:01 +0530

    Update home_employee.dart

--- lib/screens/home_employee/home_employee.dart
+++ lib/screens/home_employee/home_employee.dart
@@ -2,7 +2,7 @@ import 'package:domestic_pal/screens/hire/hire.dart';
 import 'package:domestic_pal/services/auth_employee.dart';
 import 'package:flutter/material.dart';
 
-class HomeEmployee extends StatelessWidget {
+/*class HomeEmployee extends StatelessWidget {
   final AuthService _auth = AuthService();
 
   @override
@@ -29,4 +29,113 @@ class HomeEmployee extends StatelessWidget {
       ),
     );
   }
+}*/
+class HomeEmployee extends StatefulWidget {
+  @override
+  _home_employeeState createState() => _home_employeeState();
 }
+
+class _home_employeeState extends State<HomeEmployee> {
+  int index=0;
+  List<Widget> list = [
+    ViewDetails(),
+    UpdateDetails(),
+  ];
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Navigation Drawer"),
+        ),
+        body: list[index],
+        drawer: MyDrawer(onTap: (ctx,i){
+          setState(() {
+            index=i;
+            Navigator.pop(ctx);
+          });
+        },),
+      ),
+    );
+  }
+}
+
+class MyDrawer extends StatelessWidget {
+
+  final Function onTap;
+
+  MyDrawer({
+    this.onTap
+});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width*0.8,
+      child: Drawer(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(color: Colors.blue),
+              child: Padding(
+                padding: EdgeInsets.all(6),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Container(
+                      width: 60,
+                      height: 60,
+                      child: CircleAvatar(
+
+                      ),
+                    ),
+                    SizedBox(height: 15,),
+                    Text(
+                      'Username from firebase',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white
+                      ),
+                    ),
+                    SizedBox(height: 3),
+                    Text(
+                      'Mail ID from firebase',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12
+                      ),
+                    ),
+
+                  ],
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.remove_red_eye),
+              title: Text('View Details'),
+              onTap: ()=>onTap(context,0),
+            ),
+            ListTile(
+              leading: Icon(Icons.update_outlined),
+              title: Text('Update Details'),
+              onTap: ()=>onTap(context,1),
+            ),
+            Divider(height: 1),
+            ListTile(
+              leading: Icon(Icons.exit_to_app),
+              title: Text('Logout'),
+              onTap: ()=>onTap(context,0),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
