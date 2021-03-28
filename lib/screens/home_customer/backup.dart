8c17b9fde1d91709d69ed668fe61d158e4aa47e6
Harshita-Jaya <jharshita7991@gmail.com>
2021-03-29 00:13:35 +0530

    customer home screen

---
+++ lib/screens/home_customer/backup.dart
@@ -0,0 +1,35 @@
+import 'package:domestic_pal/screens/hire/hire.dart';
+import 'package:domestic_pal/services/auth_customer.dart';
+import 'package:flutter/material.dart';
+
+class HomeCustomer extends StatelessWidget {
+  final AuthService _auth = AuthService();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.pink[200],
+        elevation: 0.0,
+        title: Text('Customer Home Screen'),
+        actions: <Widget>[
+          FlatButton.icon(
+              onPressed: () async {
+                await _auth.signOut();
+                Navigator.of(context).pushAndRemoveUntil(
+                            MaterialPageRoute(
+                                builder: (context) => Hire()),
+                            (Route<dynamic> route) => false);
+              },
+              icon: Icon(Icons.person),
+              label: Text('logout')
+            )
+        ],
+        
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+        child: Text('What are you looking for?'),
+      ),
+    );
+  }
+}
