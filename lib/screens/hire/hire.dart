3857e9d6e65d97bc8636624bb19163b1363ee573
Harshita Anand <aharshita16@gmail.com>
2021-03-28 15:03:04 +0530

    hireChanges

--- lib/screens/hire/hire.dart
+++ lib/screens/hire/hire.dart
@@ -7,48 +7,51 @@ class Hire extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Colors.blueGrey[100],
         body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        //crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            'Do you want to:',
-            style: TextStyle(fontSize: 30.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            //crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Do you want to:',
+                style: TextStyle(fontSize: 30.0),
+              ),
+              SizedBox(height: 20.0),
+              //FOR CUSTOMERS
+              FlatButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => AuthenticateCustomer()),
+                  );
+                },
+                minWidth: 115.0,
+                color: Colors.amber[400],
+                child: Text(
+                  'Hire',
+                  style: TextStyle(fontSize: 20.0),
+                ),
+              ),
+              //FOR EMPLOYEES
+              FlatButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => AuthenticateEmployee()),
+                  );
+                },
+                minWidth: 60.0,
+                color: Colors.cyan[400],
+                child: Text(
+                  'Get hired',
+                  style: TextStyle(fontSize: 20.0),
+                ),
+              )
+            ],
           ),
-          SizedBox(height: 20.0),
-          //FOR CUSTOMERS
-          FlatButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => AuthenticateCustomer()),
-              );
-            },
-            minWidth: 115.0,
-            color: Colors.red[800],
-            child: Text(
-              'Hire',
-              style: TextStyle(fontSize: 20.0),
-            ),
-          ),
-          //FOR EMPLOYEES
-          FlatButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => AuthenticateEmployee()),
-              );
-            },
-            minWidth: 60.0,
-            color: Colors.amber[800],
-            child: Text(
-              'Get hired',
-              style: TextStyle(fontSize: 20.0),
-            ),
-          )
-        ],
-      ),
-    ));
+        ));
   }
 }
