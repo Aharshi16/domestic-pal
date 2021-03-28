bdd8fcad8ccb16cbef84fd728db98f6b980e46a8
Harshita Anand <aharshita16@gmail.com>
2021-03-28 20:07:52 +0530

    loading screen

--- lib/screens/authenticate/register_employee.dart
+++ lib/screens/authenticate/register_employee.dart
@@ -1,4 +1,5 @@
 import 'package:domestic_pal/screens/home_employee/home_employee.dart';
+import 'package:domestic_pal/shared/loading.dart';
 import 'package:flutter/material.dart';
 import 'package:domestic_pal/services/auth_employee.dart';
 import 'package:domestic_pal/shared/constants.dart';
@@ -15,6 +16,7 @@ class _RegisterEmployeeState extends State<RegisterEmployee> {
   final AuthService _auth = AuthService();
 
   final _formKey = GlobalKey<FormState>();
+  bool loading = false;
 
   String email = '';
   String password = '';
@@ -22,93 +24,100 @@ class _RegisterEmployeeState extends State<RegisterEmployee> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.cyan[50],
-      appBar: AppBar(
-        backgroundColor: Colors.cyan[800],
-        elevation: 0.0,
-        title: Text('domesticPal'),
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 20.0),
-              Text(
-                'Register as an Employee',
-                style: TextStyle(color: Colors.cyan[800], fontSize: 20.0),
-              ),
-              SizedBox(height: 40.0),
-              TextFormField(
-                decoration: textInputDecorationEmp.copyWith(hintText: 'Email'),
-                validator: (val) => val.isEmpty ? 'Enter an email' : null,
-                onChanged: (val) {
-                  setState(() => email = val);
-                },
-              ),
-              SizedBox(height: 20.0),
-              TextFormField(
-                obscureText: true,
-                decoration:
-                    textInputDecorationEmp.copyWith(hintText: 'Password'),
-                validator: (val) => val.length < 6 ? 'Enter a password' : null,
-                onChanged: (val) {
-                  setState(() => password = val);
-                },
-              ),
-              SizedBox(height: 20.0),
-              RaisedButton(
-                color: Colors.cyan[800],
-                child: Text(
-                  'Register',
-                  style: TextStyle(color: Colors.white),
-                ),
-                onPressed: () async {
-                  if (_formKey.currentState.validate()) {
-                    dynamic result = await _auth
-                        .registerWithEmailAndPasswordEmployee(email, password);
-                    if (result == null) {
-                      setState(() =>
-                          error = 'could not register with those credentials');
-                    } else {
-                      setState(() => error = 'Registered successfully');
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.cyan[50],
+            appBar: AppBar(
+              backgroundColor: Colors.cyan[800],
+              elevation: 0.0,
+              title: Text('domesticPal'),
+            ),
+            body: Container(
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(height: 20.0),
+                    Text(
+                      'Register as an Employee',
+                      style: TextStyle(color: Colors.cyan[800], fontSize: 20.0),
+                    ),
+                    SizedBox(height: 40.0),
+                    TextFormField(
+                      decoration:
+                          textInputDecorationEmp.copyWith(hintText: 'Email'),
+                      validator: (val) => val.isEmpty ? 'Enter an email' : null,
+                      onChanged: (val) {
+                        setState(() => email = val);
+                      },
+                    ),
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      obscureText: true,
+                      decoration:
+                          textInputDecorationEmp.copyWith(hintText: 'Password'),
+                      validator: (val) =>
+                          val.length < 6 ? 'Enter a password' : null,
+                      onChanged: (val) {
+                        setState(() => password = val);
+                      },
+                    ),
+                    SizedBox(height: 20.0),
+                    RaisedButton(
+                      color: Colors.cyan[800],
+                      child: Text(
+                        'Register',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () async {
+                        if (_formKey.currentState.validate()) {
+                          setState(() => loading = true);
+                          dynamic result =
+                              await _auth.registerWithEmailAndPasswordEmployee(
+                                  email, password);
+                          if (result == null) {
+                            setState(() => error =
+                                'could not register with those credentials');
+                            loading = false;
+                          } else {
+                            setState(() => error = 'Registered successfully');
 
-                      Navigator.of(context).pushAndRemoveUntil(
-                          MaterialPageRoute(
-                              builder: (context) => HomeEmployee()),
-                          (Route<dynamic> route) => false);
-                    }
-                  }
-                },
+                            Navigator.of(context).pushAndRemoveUntil(
+                                MaterialPageRoute(
+                                    builder: (context) => HomeEmployee()),
+                                (Route<dynamic> route) => false);
+                          }
+                        }
+                      },
+                    ),
+                    SizedBox(height: 12.0),
+                    Text(
+                      error,
+                      style: TextStyle(color: Colors.red, fontSize: 14.0),
+                    ),
+                    SizedBox(height: 5.0),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Have an account?',
+                          style: TextStyle(color: Colors.grey, fontSize: 14.0),
+                        ),
+                        FlatButton(
+                          child: Text('Sign In',
+                              style: TextStyle(color: Colors.cyan)),
+                          onPressed: () {
+                            widget.toggleView();
+                          },
+                        )
+                      ],
+                    )
+                  ],
+                ),
               ),
-              SizedBox(height: 12.0),
-              Text(
-                error,
-                style: TextStyle(color: Colors.red, fontSize: 14.0),
-              ),
-              SizedBox(height: 5.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Have an account?',
-                    style: TextStyle(color: Colors.grey, fontSize: 14.0),
-                  ),
-                  FlatButton(
-                    child:
-                        Text('Sign In', style: TextStyle(color: Colors.cyan)),
-                    onPressed: () {
-                      widget.toggleView();
-                    },
-                  )
-                ],
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
   }
 }
--- lib/screens/authenticate/signin_employee.dart
+++ lib/screens/authenticate/signin_employee.dart
@@ -1,5 +1,6 @@
 import 'package:domestic_pal/screens/home_employee/home_employee.dart';
 import 'package:domestic_pal/services/auth_employee.dart';
+import 'package:domestic_pal/shared/loading.dart';
 import 'package:flutter/material.dart';
 import 'package:domestic_pal/shared/constants.dart';
 
@@ -14,6 +15,7 @@ class SignInEmployee extends StatefulWidget {
 class _SignInEmployeeState extends State<SignInEmployee> {
   final AuthService _authEmployee = AuthService();
   final _formKey = GlobalKey<FormState>();
+  bool loading = false;
 
   //text field state
   String email = '';
@@ -22,93 +24,100 @@ class _SignInEmployeeState extends State<SignInEmployee> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.cyan[50],
-      appBar: AppBar(
-        backgroundColor: Colors.cyan[800],
-        elevation: 0.0,
-        title: Text('domesticPal'),
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 20.0),
-              Text(
-                'Sign In as an Employee',
-                style: TextStyle(color: Colors.cyan[800], fontSize: 20.0),
-              ),
-              SizedBox(height: 40.0),
-              TextFormField(
-                decoration: textInputDecorationEmp.copyWith(hintText: 'Email'),
-                validator: (val) => val.isEmpty ? 'Enter an email' : null,
-                onChanged: (val) {
-                  setState(() => email = val);
-                },
-              ),
-              SizedBox(height: 20.0),
-              TextFormField(
-                obscureText: true,
-                decoration:
-                    textInputDecorationEmp.copyWith(hintText: 'Password'),
-                validator: (val) => val.length < 6 ? 'Enter a password' : null,
-                onChanged: (val) {
-                  setState(() => password = val);
-                },
-              ),
-              SizedBox(height: 20.0),
-              RaisedButton(
-                  color: Colors.cyan[800],
-                  child: Text(
-                    'Sign in',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  onPressed: () async {
-                    if (_formKey.currentState.validate()) {
-                      dynamic result = await _authEmployee
-                          .signInWithEmailAndPasswordEmployee(email, password);
-                      /*dynamic result = await _authEmployee
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.cyan[50],
+            appBar: AppBar(
+              backgroundColor: Colors.cyan[800],
+              elevation: 0.0,
+              title: Text('domesticPal'),
+            ),
+            body: Container(
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(height: 20.0),
+                    Text(
+                      'Sign In as an Employee',
+                      style: TextStyle(color: Colors.cyan[800], fontSize: 20.0),
+                    ),
+                    SizedBox(height: 40.0),
+                    TextFormField(
+                      decoration:
+                          textInputDecorationEmp.copyWith(hintText: 'Email'),
+                      validator: (val) => val.isEmpty ? 'Enter an email' : null,
+                      onChanged: (val) {
+                        setState(() => email = val);
+                      },
+                    ),
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      obscureText: true,
+                      decoration:
+                          textInputDecorationEmp.copyWith(hintText: 'Password'),
+                      validator: (val) =>
+                          val.length < 6 ? 'Enter a password' : null,
+                      onChanged: (val) {
+                        setState(() => password = val);
+                      },
+                    ),
+                    SizedBox(height: 20.0),
+                    RaisedButton(
+                        color: Colors.cyan[800],
+                        child: Text(
+                          'Sign in',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        onPressed: () async {
+                          if (_formKey.currentState.validate()) {
+                            setState(() => loading = true);
+                            dynamic result = await _authEmployee
+                                .signInWithEmailAndPasswordEmployee(
+                                    email, password);
+                            /*dynamic result = await _authEmployee
                           .signInWithEmailAndPasswordEmployee(email, password);*/
-                      if (result == null) {
-                        setState(() =>
-                            error = 'could not sign in with those credentials');
-                      } else {
-                        setState(() => error = 'logged in successfully');
-                        Navigator.of(context).pushAndRemoveUntil(
-                            MaterialPageRoute(
-                                builder: (context) => HomeEmployee()),
-                            (Route<dynamic> route) => false);
-                      }
-                    }
-                  }),
-              SizedBox(height: 12.0),
-              Text(
-                error,
-                style: TextStyle(color: Colors.red, fontSize: 14.0),
+                            if (result == null) {
+                              setState(() => error =
+                                  'could not sign in with those credentials');
+                              loading = false;
+                            } else {
+                              setState(() => error = 'logged in successfully');
+                              Navigator.of(context).pushAndRemoveUntil(
+                                  MaterialPageRoute(
+                                      builder: (context) => HomeEmployee()),
+                                  (Route<dynamic> route) => false);
+                            }
+                          }
+                        }),
+                    SizedBox(height: 12.0),
+                    Text(
+                      error,
+                      style: TextStyle(color: Colors.red, fontSize: 14.0),
+                    ),
+                    SizedBox(height: 5.0),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'New user?',
+                          style: TextStyle(color: Colors.grey, fontSize: 14.0),
+                        ),
+                        FlatButton(
+                          child: Text('Register',
+                              style: TextStyle(color: Colors.cyan)),
+                          onPressed: () {
+                            widget.toggleView();
+                          },
+                        )
+                      ],
+                    )
+                  ],
+                ),
               ),
-              SizedBox(height: 5.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'New user?',
-                    style: TextStyle(color: Colors.grey, fontSize: 14.0),
-                  ),
-                  FlatButton(
-                    child:
-                        Text('Register', style: TextStyle(color: Colors.cyan)),
-                    onPressed: () {
-                      widget.toggleView();
-                    },
-                  )
-                ],
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
   }
 }
