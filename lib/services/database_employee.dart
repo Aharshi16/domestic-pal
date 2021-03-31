5fb1bcda60cf68d727f428af7aee79c598b88558
Harshita Anand <aharshita16@gmail.com>
2021-03-31 21:12:01 +0530

    userDataEmp

--- lib/services/database_employee.dart
+++ lib/services/database_employee.dart
@@ -1,7 +1,31 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseService {
+  final String uid;
+  DatabaseService({this.uid});
+
   //Employee collection reference
   final CollectionReference employeeCollection =
       Firestore.instance.collection('empDetails');
+
+  Future updateUserData(
+      String userCatagory,
+      String name,
+      int phoneNo,
+      String gender,
+      String location,
+      int workExperience,
+      int rating,
+      String jobProfile) async {
+    return await employeeCollection.document(uid).setData({
+      'userCatagory': userCatagory,
+      'name': name,
+      'phoneNo': phoneNo,
+      'gender': gender,
+      'location': location,
+      'workExperience': workExperience,
+      'rating': rating,
+      'jobProfile': jobProfile
+    });
+  }
 }
