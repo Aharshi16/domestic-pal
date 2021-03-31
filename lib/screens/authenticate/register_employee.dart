ffbfc4bf7ec5c8e72f1f71821ddb37b06c252542
Harshita Anand <aharshita16@gmail.com>
2021-03-31 15:21:04 +0530

    screenoverlayErrorRemoved

--- lib/screens/authenticate/register_employee.dart
+++ lib/screens/authenticate/register_employee.dart
@@ -33,88 +33,93 @@ class _RegisterEmployeeState extends State<RegisterEmployee> {
               elevation: 0.0,
               title: Text('domesticPal'),
             ),
-            body: Container(
-              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  children: <Widget>[
-                    SizedBox(height: 20.0),
-                    Text(
-                      'Register as an Employee',
-                      style: TextStyle(color: Colors.cyan[800], fontSize: 20.0),
-                    ),
-                    SizedBox(height: 40.0),
-                    TextFormField(
-                      decoration:
-                          textInputDecorationEmp.copyWith(hintText: 'Email'),
-                      validator: (val) => val.isEmpty ? 'Enter an email' : null,
-                      onChanged: (val) {
-                        setState(() => email = val);
-                      },
-                    ),
-                    SizedBox(height: 20.0),
-                    TextFormField(
-                      obscureText: true,
-                      decoration:
-                          textInputDecorationEmp.copyWith(hintText: 'Password'),
-                      validator: (val) =>
-                          val.length < 6 ? 'Enter a password' : null,
-                      onChanged: (val) {
-                        setState(() => password = val);
-                      },
-                    ),
-                    SizedBox(height: 20.0),
-                    RaisedButton(
-                      color: Colors.cyan[800],
-                      child: Text(
-                        'Register',
-                        style: TextStyle(color: Colors.white),
+            body: SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(height: 40.0),
+                      Text(
+                        'Register as an Employee',
+                        style:
+                            TextStyle(color: Colors.cyan[800], fontSize: 20.0),
                       ),
-                      onPressed: () async {
-                        if (_formKey.currentState.validate()) {
-                          setState(() => loading = true);
-                          dynamic result =
-                              await _auth.registerWithEmailAndPasswordEmployee(
-                                  email, password);
-                          if (result == null) {
-                            setState(() => error =
-                                'could not register with those credentials');
-                            loading = false;
-                          } else {
-                            setState(() => error = 'Registered successfully');
-
-                            Navigator.of(context).pushAndRemoveUntil(
-                                MaterialPageRoute(
-                                    builder: (context) => HomeEmployee()),
-                                (Route<dynamic> route) => false);
-                          }
-                        }
-                      },
-                    ),
-                    SizedBox(height: 12.0),
-                    Text(
-                      error,
-                      style: TextStyle(color: Colors.red, fontSize: 14.0),
-                    ),
-                    SizedBox(height: 5.0),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Have an account?',
-                          style: TextStyle(color: Colors.grey, fontSize: 14.0),
+                      SizedBox(height: 40.0),
+                      TextFormField(
+                        decoration:
+                            textInputDecorationEmp.copyWith(hintText: 'Email'),
+                        validator: (val) =>
+                            val.isEmpty ? 'Enter an email' : null,
+                        onChanged: (val) {
+                          setState(() => email = val);
+                        },
+                      ),
+                      SizedBox(height: 20.0),
+                      TextFormField(
+                        obscureText: true,
+                        decoration: textInputDecorationEmp.copyWith(
+                            hintText: 'Password'),
+                        validator: (val) =>
+                            val.length < 6 ? 'Enter a password' : null,
+                        onChanged: (val) {
+                          setState(() => password = val);
+                        },
+                      ),
+                      SizedBox(height: 40.0),
+                      RaisedButton(
+                        color: Colors.cyan[800],
+                        child: Text(
+                          'Register',
+                          style: TextStyle(color: Colors.white),
                         ),
-                        FlatButton(
-                          child: Text('Sign In',
-                              style: TextStyle(color: Colors.cyan)),
-                          onPressed: () {
-                            widget.toggleView();
-                          },
-                        )
-                      ],
-                    )
-                  ],
+                        onPressed: () async {
+                          if (_formKey.currentState.validate()) {
+                            setState(() => loading = true);
+                            dynamic result = await _auth
+                                .registerWithEmailAndPasswordEmployee(
+                                    email, password);
+                            if (result == null) {
+                              setState(() => error =
+                                  'could not register with those credentials');
+                              loading = false;
+                            } else {
+                              setState(() => error = 'Registered successfully');
+
+                              Navigator.of(context).pushAndRemoveUntil(
+                                  MaterialPageRoute(
+                                      builder: (context) => HomeEmployee()),
+                                  (Route<dynamic> route) => false);
+                            }
+                          }
+                        },
+                      ),
+                      SizedBox(height: 15.0),
+                      Text(
+                        error,
+                        style: TextStyle(color: Colors.red, fontSize: 14.0),
+                      ),
+                      SizedBox(height: 5.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Have an account?',
+                            style:
+                                TextStyle(color: Colors.grey, fontSize: 14.0),
+                          ),
+                          FlatButton(
+                            child: Text('Sign In',
+                                style: TextStyle(color: Colors.cyan)),
+                            onPressed: () {
+                              widget.toggleView();
+                            },
+                          )
+                        ],
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
