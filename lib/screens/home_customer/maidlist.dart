bbcaf5ea7338f2541fedcad5270b24e73f25d0ee
Harshita-Jaya <jharshita7991@gmail.com>
2021-04-10 20:20:44 +0530

    typo

--- lib/screens/home_customer/maidlist.dart
+++ lib/screens/home_customer/maidlist.dart
@@ -6,27 +6,28 @@ class Maidlist extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.pink[300],
-        elevation: 0.0,
-        title: Text('Maild List'),
-      ),
-       
+        appBar: AppBar(
+          backgroundColor: Colors.pink[300],
+          elevation: 0.0,
+          title: Text('Maid List'),
+        ),
         body: StreamBuilder(
-          stream: Firestore.instance.collection("empDetails").where("jobProfile", isEqualTo: "Maid").snapshots(),
-          builder: (context, snapshot){
-
-            if(snapshot.data == null) return CusLoading();
+          stream: Firestore.instance
+              .collection("empDetails")
+              .where("jobProfile", isEqualTo: "Maid")
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.data == null) return CusLoading();
 
             return ListView.builder(
               itemCount: snapshot.data.documents.length,
-              itemBuilder: (context, index){
+              itemBuilder: (context, index) {
                 DocumentSnapshot empdetails = snapshot.data.documents[index];
                 return ListTile(
                   title: Text(empdetails['name'] ?? 'default'),
                   //subtitle: empdetails['rating'] ?? 'default',
                 );
-               /* return Card(
+                /* return Card(
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
@@ -37,7 +38,6 @@ class Maidlist extends StatelessWidget {
               },
             );
           },
-        )
-    );
+        ));
   }
 }
