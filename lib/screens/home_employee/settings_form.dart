e31b46d3a76856524fa2d3284f8970a90a19fc79
Harshita-Jaya <jharshita7991@gmail.com>
2021-05-20 13:05:47 +0530

    emp setting form

--- lib/screens/home_employee/settings_form.dart
+++ lib/screens/home_employee/settings_form.dart
@@ -4,14 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:domestic_pal/shared/constants.dart';
 
-
 class SettingsForm extends StatefulWidget {
   @override
   _SettingsFormState createState() => _SettingsFormState();
 }
 
 class _SettingsFormState extends State<SettingsForm> {
-
   final _formkey = GlobalKey<FormState>();
 
   String _currentname;
@@ -25,58 +23,76 @@ class _SettingsFormState extends State<SettingsForm> {
 
   @override
   Widget build(BuildContext context) {
+    final bottom = MediaQuery.of(context).viewInsets.bottom;
     final user = Provider.of<User>(context);
     return StreamBuilder<EmployeeUserData>(
         stream: DatabaseEmployeeService(uid: user.uid).empDetails,
         builder: (context, snapshot) {
           //if (snapshot.hasData) {
-            EmployeeUserData userData = snapshot.data;
-            return Form(
-              key: _formkey,
-              child: Column(
-                children: <Widget>[
-                  Text('Update your Settings'),
-                  SizedBox(height: 10.0),
-                  TextFormField(
-                    decoration: textInputDecorationEmp,
-                    initialValue: userData.name,
-                    validator: (value) => value.isEmpty ? 'Please enter a name' : null,
-                    onChanged: (value) => setState(() => _currentname = value),
-                  ),
-                  SizedBox(height: 10.0),
-                  TextFormField(
-                    decoration: textInputDecorationEmp,
-                    initialValue: userData.phoneNo,
-                    validator: (value) => value.isEmpty ? 'Please enter a Phone Number' : null,
-                    onChanged: (value) => setState(() => _currentPhone = value),
-                  ),
-                  SizedBox(height: 10.0),
-                  TextFormField(
-                    decoration: textInputDecorationEmp,
-                    initialValue: userData.location,
-                    validator: (value) => value.isEmpty ? 'Please enter a location' : null,
-                    onChanged: (value) => setState(() => _currentLocation = value),
-                  ),
-                  SizedBox(height: 10.0),
-                  TextFormField(
-                    decoration: textInputDecorationEmp,
-                    initialValue: userData.aadharNo,
-                    validator: (value) => value.isEmpty ? 'Please enter an aadhar number' : null,
-                    onChanged: (value) => setState(() => _currentaadhar = value),
-                  ),
-                  SizedBox(height: 10.0),
-                  TextFormField(
-                    decoration: textInputDecorationEmp,
-                    initialValue: userData.workExperience,
-                    validator: (value) => value.isEmpty ? 'Please enter your work experience' : null,
-                    onChanged: (value) => setState(() => _currentworkExperience = value),
-                  ),
-                  SizedBox(height: 20.0),
-                  RaisedButton(
+          EmployeeUserData userData = snapshot.data;
+          return SingleChildScrollView(
+            reverse: true,
+            child: Padding(
+              padding: EdgeInsets.only(bottom: bottom),
+              child: Form(
+                key: _formkey,
+                child: Column(
+                  children: <Widget>[
+                    Text('Update your Settings'),
+                    SizedBox(height: 10.0),
+                    TextFormField(
+                      decoration: textInputDecorationEmp,
+                      initialValue: userData.name,
+                      validator: (value) =>
+                          value.isEmpty ? 'Please enter a name' : null,
+                      onChanged: (value) =>
+                          setState(() => _currentname = value),
+                    ),
+                    SizedBox(height: 10.0),
+                    TextFormField(
+                      decoration: textInputDecorationEmp,
+                      initialValue: userData.phoneNo,
+                      validator: (value) =>
+                          value.isEmpty ? 'Please enter a Phone Number' : null,
+                      onChanged: (value) =>
+                          setState(() => _currentPhone = value),
+                    ),
+                    SizedBox(height: 10.0),
+                    TextFormField(
+                      decoration: textInputDecorationEmp,
+                      initialValue: userData.location,
+                      validator: (value) =>
+                          value.isEmpty ? 'Please enter a location' : null,
+                      onChanged: (value) =>
+                          setState(() => _currentLocation = value),
+                    ),
+                    SizedBox(height: 10.0),
+                    TextFormField(
+                      decoration: textInputDecorationEmp,
+                      initialValue: userData.aadharNo,
+                      validator: (value) => value.isEmpty
+                          ? 'Please enter an aadhar number'
+                          : null,
+                      onChanged: (value) =>
+                          setState(() => _currentaadhar = value),
+                    ),
+                    SizedBox(height: 10.0),
+                    TextFormField(
+                      decoration: textInputDecorationEmp,
+                      initialValue: userData.workExperience,
+                      validator: (value) => value.isEmpty
+                          ? 'Please enter your work experience'
+                          : null,
+                      onChanged: (value) =>
+                          setState(() => _currentworkExperience = value),
+                    ),
+                    SizedBox(height: 20.0),
+                    RaisedButton(
                       onPressed: () async {
-                        if(_formkey.currentState.validate()) {
-                          await DatabaseEmployeeService(uid: user.uid).updateEmployeeUserData(
-                              'E',
+                        if (_formkey.currentState.validate()) {
+                          await DatabaseEmployeeService(uid: user.uid)
+                              .updateEmployeeUserData(
+                            'E',
                             _currentname ?? userData.name,
                             _currentPhone ?? userData.phoneNo,
                             _currentGender ?? userData.gender,
@@ -89,24 +105,22 @@ class _SettingsFormState extends State<SettingsForm> {
                         }
                         Navigator.pop(context);
                       },
-                    color: Colors.amber,
-                    child: Text(
-                      'Update',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  )
-                ],
+                      color: Colors.amber,
+                      child: Text(
+                        'Update',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    )
+                  ],
+                ),
               ),
-            );
+            ),
+          );
 
-
-        //}
+          //}
           /*else {
             return emploading();
           }*/
-        }
-
-    );
+        });
   }
 }
-
